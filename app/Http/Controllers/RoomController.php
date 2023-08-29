7a80e07b640542a97c591aa3aeeae0b0dc4f93ea
beruangdev <beruangdev@gmail.com>
2023-08-30 04:33:29 +0700

    update

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -217,6 +217,7 @@ class RoomController extends Controller
         //     Cookie::forever("uuid", $uuid);
         // }
         $uuid = isset($_COOKIE["uuid"]) ? $_COOKIE["uuid"] : null;
+        dd($uuid);
         if (!$uuid) {
             $uuid = Str::uuid();
             setcookie("uuid", $uuid, time() + (5 * 365 * 24 * 60 * 60), "/", "roommeet.fun", false, false);
