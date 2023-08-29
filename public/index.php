50eb61d9f3c68bb3f23598f7ff21ddd79b54b54e
beruangdev <beruangdev@gmail.com>
2023-08-30 02:58:03 +0700

    update index

--- public/index.php
+++ public/index.php
@@ -34,9 +34,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// require __DIR__.'/bootstrap/autoload.php';
-// $app = require_once __DIR__.'/bootstrap/app.php';
-
 $kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
